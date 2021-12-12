32ae9fcdd8af3907fda0d8a4a4ab68050afa1c4b
itpark-team <46758840+itpark-team@users.noreply.github.com>
2021-12-12 18:58:04 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsAppLibrary/Views/FormMain.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace WindowsFormsAppLibrary
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxFilteredAuthors = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonChooseBooksByAuthor = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxBookName = new System.Windows.Forms.TextBox();
+            this.textBoxBookPrice = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxBookAuthor = new System.Windows.Forms.ComboBox();
+            this.buttonAddBook = new System.Windows.Forms.Button();
+            this.textBoxBookId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonDeleteBook = new System.Windows.Forms.Button();
+            this.buttonClearFields = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewBooks
+            // 
+            this.dataGridViewBooks.AllowUserToAddRows = false;
+            this.dataGridViewBooks.AllowUserToDeleteRows = false;
+            this.dataGridViewBooks.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBooks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridViewBooks.Location = new System.Drawing.Point(16, 48);
+            this.dataGridViewBooks.Name = "dataGridViewBooks";
+            this.dataGridViewBooks.ReadOnly = true;
+            this.dataGridViewBooks.Size = new System.Drawing.Size(536, 224);
+            this.dataGridViewBooks.TabIndex = 0;
+            this.dataGridViewBooks.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewBooks_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Название книги";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Цена";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Автор";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Книги";
+            // 
+            // comboBoxFilteredAuthors
+            // 
+            this.comboBoxFilteredAuthors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFilteredAuthors.FormattingEnabled = true;
+            this.comboBoxFilteredAuthors.Location = new System.Drawing.Point(568, 48);
+            this.comboBoxFilteredAuthors.Name = "comboBoxFilteredAuthors";
+            this.comboBoxFilteredAuthors.Size = new System.Drawing.Size(224, 21);
+            this.comboBoxFilteredAuthors.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(568, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Авторы";
+            // 
+            // buttonChooseBooksByAuthor
+            // 
+            this.buttonChooseBooksByAuthor.Location = new System.Drawing.Point(568, 80);
+            this.buttonChooseBooksByAuthor.Name = "buttonChooseBooksByAuthor";
+            this.buttonChooseBooksByAuthor.Size = new System.Drawing.Size(224, 23);
+            this.buttonChooseBooksByAuthor.TabIndex = 4;
+            this.buttonChooseBooksByAuthor.Text = "Выбрать книги по автору";
+            this.buttonChooseBooksByAuthor.UseVisualStyleBackColor = true;
+            this.buttonChooseBooksByAuthor.Click += new System.EventHandler(this.buttonChooseBooksByAuthor_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Название книги";
+            // 
+            // textBoxBookName
+            // 
+            this.textBoxBookName.Location = new System.Drawing.Point(16, 328);
+            this.textBoxBookName.Name = "textBoxBookName";
+            this.textBoxBookName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxBookName.TabIndex = 6;
+            // 
+            // textBoxBookPrice
+            // 
+            this.textBoxBookPrice.Location = new System.Drawing.Point(224, 328);
+            this.textBoxBookPrice.Name = "textBoxBookPrice";
+            this.textBoxBookPrice.Size = new System.Drawing.Size(128, 20);
+            this.textBoxBookPrice.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Цена";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(360, 312);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Автор";
+            // 
+            // comboBoxBookAuthor
+            // 
+            this.comboBoxBookAuthor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBookAuthor.FormattingEnabled = true;
+            this.comboBoxBookAuthor.Location = new System.Drawing.Point(360, 328);
+            this.comboBoxBookAuthor.Name = "comboBoxBookAuthor";
+            this.comboBoxBookAuthor.Size = new System.Drawing.Size(192, 21);
+            this.comboBoxBookAuthor.TabIndex = 10;
+            // 
+            // buttonAddBook
+            // 
+            this.buttonAddBook.Location = new System.Drawing.Point(16, 384);
+            this.buttonAddBook.Name = "buttonAddBook";
+            this.buttonAddBook.Size = new System.Drawing.Size(136, 23);
+            this.buttonAddBook.TabIndex = 11;
+            this.buttonAddBook.Text = "Добавить книгу";
+            this.buttonAddBook.UseVisualStyleBackColor = true;
+            this.buttonAddBook.Click += new System.EventHandler(this.buttonAddBook_Click);
+            // 
+            // textBoxBookId
+            // 
+            this.textBoxBookId.Location = new System.Drawing.Point(16, 288);
+            this.textBoxBookId.Name = "textBoxBookId";
+            this.textBoxBookId.ReadOnly = true;
+            this.textBoxBookId.Size = new System.Drawing.Size(200, 20);
+            this.textBoxBookId.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Id книги";
+            // 
+            // buttonDeleteBook
+            // 
+            this.buttonDeleteBook.Location = new System.Drawing.Point(160, 384);
+            this.buttonDeleteBook.Name = "buttonDeleteBook";
+            this.buttonDeleteBook.Size = new System.Drawing.Size(136, 23);
+            this.buttonDeleteBook.TabIndex = 14;
+            this.buttonDeleteBook.Text = "Удалить книгу";
+            this.buttonDeleteBook.UseVisualStyleBackColor = true;
+            this.buttonDeleteBook.Click += new System.EventHandler(this.buttonDeleteBook_Click);
+            // 
+            // buttonClearFields
+            // 
+            this.buttonClearFields.Location = new System.Drawing.Point(304, 384);
+            this.buttonClearFields.Name = "buttonClearFields";
+            this.buttonClearFields.Size = new System.Drawing.Size(152, 23);
+            this.buttonClearFields.TabIndex = 15;
+            this.buttonClearFields.Text = "Очистить поля";
+            this.buttonClearFields.UseVisualStyleBackColor = true;
+            this.buttonClearFields.Click += new System.EventHandler(this.buttonClearFields_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClearFields);
+            this.Controls.Add(this.buttonDeleteBook);
+            this.Controls.Add(this.textBoxBookId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonAddBook);
+            this.Controls.Add(this.comboBoxBookAuthor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxBookPrice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxBookName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonChooseBooksByAuthor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxFilteredAuthors);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewBooks);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewBooks;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxFilteredAuthors;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonChooseBooksByAuthor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxBookName;
+        private System.Windows.Forms.TextBox textBoxBookPrice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxBookAuthor;
+        private System.Windows.Forms.Button buttonAddBook;
+        private System.Windows.Forms.TextBox textBoxBookId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonDeleteBook;
+        private System.Windows.Forms.Button buttonClearFields;
+    }
+}
+
---
+++ WindowsFormsAppLibrary/Views/FormMain.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsAppLibrary.Models;
+
+namespace WindowsFormsAppLibrary
+{
+    public partial class FormMain : Form
+    {
+        private LibraryEntities libraryEntities;
+
+        private void UpdateDataGridViewBooks(List<Book> books)
+        {
+            dataGridViewBooks.Rows.Clear();
+
+            foreach (Book book in books)
+            {
+                dataGridViewBooks.Rows.Add(
+                    book.Id,
+                    book.Name,
+                    book.Price,
+                    book.Author.FirstName + " " + book.Author.LastName);
+            }
+        }
+
+        private void UpdateComboBoxAuthors()
+        {
+            comboBoxFilteredAuthors.Items.Clear();
+
+            List<Author> authors = libraryEntities.Authors.ToList();
+
+            comboBoxFilteredAuthors.Items.Add("Все");
+
+            foreach (Author author in authors)
+            {
+                comboBoxFilteredAuthors.Items.Add(author.FirstName + " " + author.LastName);
+            }
+
+            comboBoxFilteredAuthors.SelectedIndex = 0;
+        }
+
+        private void UpdateComboBoxBookAuthor()
+        {
+            comboBoxBookAuthor.Items.Clear();
+
+            List<Author> authors = libraryEntities.Authors.ToList();
+
+            foreach (Author author in authors)
+            {
+                comboBoxBookAuthor.Items.Add(author.FirstName + " " + author.LastName);
+            }
+
+            comboBoxBookAuthor.SelectedIndex = -1;
+        }
+
+        private void ClearBookFields()
+        {
+            textBoxBookId.Clear();
+            textBoxBookName.Clear();
+            textBoxBookPrice.Clear();
+            comboBoxBookAuthor.SelectedIndex = -1;
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            libraryEntities = new LibraryEntities();
+
+            List<Book> books = libraryEntities.Books.ToList();
+            UpdateDataGridViewBooks(books);
+            UpdateComboBoxAuthors();
+            UpdateComboBoxBookAuthor();
+        }
+
+        private void buttonChooseBooksByAuthor_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = comboBoxFilteredAuthors.SelectedIndex;
+
+            if(selectedIndex == 0)
+            {
+                List<Book> books = libraryEntities.Books.ToList();
+                UpdateDataGridViewBooks(books);
+            }
+            else
+            {
+                List<Author> authors = libraryEntities.Authors.ToList();
+                int selectedIdAuthor = authors[selectedIndex - 1].Id;
+
+                List<Book> books = libraryEntities.Books.Where(book => book.IdAuthor == selectedIdAuthor).ToList();
+
+                UpdateDataGridViewBooks(books);
+            }
+        }
+
+        private void buttonClearFields_Click(object sender, EventArgs e)
+        {
+            ClearBookFields();
+        }
+
+        private void buttonAddBook_Click(object sender, EventArgs e)
+        {
+            string name = textBoxBookName.Text;
+            int price = int.Parse(textBoxBookPrice.Text);
+
+            int selectedIndex = comboBoxBookAuthor.SelectedIndex;
+            List<Author> authors = libraryEntities.Authors.ToList();
+            int idAuthor = authors[selectedIndex].Id;
+
+            Book book = new Book()
+            {
+                Name = name,
+                Price = price,
+                IdAuthor = idAuthor
+            };
+
+            libraryEntities.Books.Add(book);
+            libraryEntities.SaveChanges();
+
+            List<Book> books = libraryEntities.Books.ToList();
+            UpdateDataGridViewBooks(books);
+
+            comboBoxFilteredAuthors.SelectedIndex = 0;
+
+            ClearBookFields();
+        }
+
+        private void dataGridViewBooks_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewBooks.SelectedRows.Count == 0)
+            {
+                return;
+            }
+
+            int selectedIdBook = int.Parse(dataGridViewBooks.SelectedRows[0].Cells[0].Value.ToString());
+
+           Book selectedBook = libraryEntities.Books.First(book => book.Id == selectedIdBook);
+
+
+            textBoxBookId.Text = selectedBook.Id.ToString();
+            textBoxBookName.Text = selectedBook.Name;
+            textBoxBookPrice.Text = selectedBook.Price.ToString();
+
+            List<Author> authors = libraryEntities.Authors.ToList();
+            int selectedIndexAuthor = authors.FindIndex(author => author.Id == selectedBook.Author.Id);
+
+            comboBoxBookAuthor.SelectedIndex = selectedIndexAuthor;
+
+        }
+
+        private void buttonDeleteBook_Click(object sender, EventArgs e)
+        {
+            int idBook = int.Parse(textBoxBookId.Text);
+
+            Book selectedBook = libraryEntities.Books.First(book => book.Id == idBook);
+
+            libraryEntities.Books.Remove(selectedBook);
+            libraryEntities.SaveChanges();
+
+            List<Book> books = libraryEntities.Books.ToList();
+            UpdateDataGridViewBooks(books);
+
+            comboBoxFilteredAuthors.SelectedIndex = 0;
+
+            ClearBookFields();
+        }
+    }
+}
